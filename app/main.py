e57d53234047f984ad00911990b21f8f1ca18d58
Kacper Kogut <kogutkacper7@gmail.com>
2025-09-15 20:00:16 +0200

    Solution3pytest

--- app/main.py
+++ app/main.py
@@ -1,28 +1,26 @@
 class Animal:
 
-    alive = []
+    alive: list["Animal"] = []
 
     def __init__(self, name: str, health: int = 100) -> None:
         self.health = health
         self.name = name
         self.hidden = False
-        Animal.alive.append(self)
+        if self.health > 0:
+            Animal.alive.append(self)
 
     def __repr__(self) -> str:
         return (
-            f'{{Name: {self.name}, '
-            f'Health: {self.health}, '
-            f'Hidden: {self.hidden}}}'
+            f"{{Name: {self.name}, "
+            f"Health: {self.health}, "
+            f"Hidden: {self.hidden}}}"
         )
 
 
 class Herbivore(Animal):
 
     def hide(self) -> None:
-        if not self.hidden:
-            self.hidden = True
-        else:
-            self.hidden = False
+        self.hidden = not self.hidden
 
 
 class Carnivore(Animal):
